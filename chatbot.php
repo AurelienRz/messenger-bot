2a3cfd61ac3283a06accf442c5b8f5ed1029f9d7
Aurélien Robez <Aurelien@MacBook-Pro-de-Aurelien.local>
2017-03-25 22:11:26 +0100

    Simple commit

---
+++ chatbot.php
@@ -0,0 +1,18 @@
+<?php
+
+if(isset($_REQUEST['hub_challenge'])){
+  $challenge = $_REQUEST['hub_challenge'];
+  $token = $_REQUEST['hub_verify_token'];
+}
+
+if($token == "monTokenVesoul") {
+  echo $challenge;
+}
+
+$input = file_get_contents('php://input')
+
+var_dump($input);
+
+
+
+ ?>
