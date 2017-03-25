639a53e435d6d6bac005ed7159e144b4abcd0523
Aurélien Robez <Aurelien@MacBook-Pro-de-Aurelien.local>
2017-03-25 22:46:53 +0100

    Simple commit

--- chatbot.php
+++ chatbot.php
@@ -1,5 +1,7 @@
 <?php
 
+$accesToken = "EAAEFyqcYaVQBAAb53zYaneZAJKZB9LLpZAsZCIkQrBgJz4gG4LZBSHBvVZBSZAlpGs4ejWkaDbB3SiFmqccRIiTnzBiqH1ITnKvA3acjetKpNTZCerxlytc49ZAM6ZAwtZB6lVz1fLY3ZB3jUooJPLsPHEm651MA9HjWbZAzYancjrvlZAQAZDZD";
+
 if(isset($_REQUEST['hub_challenge'])){
   $challenge = $_REQUEST['hub_challenge'];
   $token = $_REQUEST['hub_verify_token'];
@@ -15,6 +17,32 @@ $userID = $input['entry'][0]['messaging'][0]['sender']['id'];
 
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-echo $userID." and ".$message; 
+echo $userID." and ".$message;
+
+
+//............
+
+
+$url = "https://graph.facebook.com/v2.6/me/messages?acces_token$accesToken";
+
+$jsonData = "{
+  'receipient': {
+    'id': $userID
+  },
+  'message': {
+    'text': 'Bonjour :)'
+  }
+}";
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_POST, true)
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+if(!empty($input['entry'][0]['messaging'][0]['message'])) {
+  curl_exec($ch);
+
+}
 
  ?>
